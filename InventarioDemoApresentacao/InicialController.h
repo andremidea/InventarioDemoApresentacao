e54e588c53d6eec75704efb37d99104bcb7bd658
Andre Midea <midea.andre@gmail.com>
2013-02-10 15:31:10 -0200

    #Parte 2

---
+++ InventarioDemoApresentacao/InicialController.h
@@ -0,0 +1,13 @@
+//
+//  InicialController.h
+//  InventarioDemoApresentacao
+//
+//  Created by André Midea Jasiskis on 10/02/13.
+//  Copyright (c) 2013 André Midea Jasiskis. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface InicialController : UIViewController
+
+@end
---
+++ InventarioDemoApresentacao/InicialController.m
@@ -0,0 +1,13 @@
+//
+//  InicialController.m
+//  InventarioDemoApresentacao
+//
+//  Created by André Midea Jasiskis on 10/02/13.
+//  Copyright (c) 2013 André Midea Jasiskis. All rights reserved.
+//
+
+#import "InicialController.h"
+
+@implementation InicialController
+
+@end
