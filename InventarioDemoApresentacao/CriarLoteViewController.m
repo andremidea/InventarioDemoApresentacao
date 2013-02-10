6af4f9a7bfdd96126963ce188c74a7e904d3f19f
Andre Midea <midea.andre@gmail.com>
2013-02-10 15:35:09 -0200

    #Parte 2

---
+++ InventarioDemoApresentacao/CriarLoteViewController.m
@@ -0,0 +1,38 @@
+//
+//  CriarLoteViewController.m
+//  InventarioDemoApresentacao
+//
+//  Created by André Midea Jasiskis on 10/02/13.
+//  Copyright (c) 2013 André Midea Jasiskis. All rights reserved.
+//
+
+#import "CriarLoteViewController.h"
+
+@interface CriarLoteViewController ()
+
+@end
+
+@implementation CriarLoteViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
