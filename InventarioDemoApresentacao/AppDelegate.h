2df4c6b5946266e3208503452d34b7f2cb01feeb
Andre Midea <midea.andre@gmail.com>
2013-02-10 14:51:55 -0200

    Initial Commit

---
+++ InventarioDemoApresentacao/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  InventarioDemoApresentacao
+//
+//  Created by André Midea Jasiskis on 10/02/13.
+//  Copyright (c) 2013 André Midea Jasiskis. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ InventarioDemoApresentacao/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  InventarioDemoApresentacao
+//
+//  Created by André Midea Jasiskis on 10/02/13.
+//  Copyright (c) 2013 André Midea Jasiskis. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
