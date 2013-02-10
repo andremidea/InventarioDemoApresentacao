15bc351e1d56dad177a3f443ac79400a2d4e7f52
Andre Midea <midea.andre@gmail.com>
2013-02-10 17:22:33 -0200

    #Parte 6

--- InventarioDemoApresentacao/InventariarViewController.m
+++ InventarioDemoApresentacao/InventariarViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "InventariarViewController.h"
+#import <QuartzCore/QuartzCore.h>
 
 @interface InventariarViewController ()
 
@@ -35,7 +36,33 @@
 }
 
 -(UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath{
-    //carregar tabela produtos
+    static NSString *CelulaProdutoCache = @"CelulaLoteIDCache";
+    UITableViewCell *celula = [self.tabelaQtdeProd dequeueReusableCellWithIdentifier:CelulaProdutoCache];
+    
+    if(!celula){
+        celula = [[[UITableViewCell alloc]initWithStyle:UITableViewCellStyleDefault reuseIdentifier:CelulaProdutoCache]autorelease];
+    }
+    celula.accessoryType = UITableViewCellAccessoryNone;
+    Produto *produto = [lote.produtos objectAtIndex:indexPath.row];
+    celula.textLabel.text = produto.nome;
+    
+    UILabel *label = [[UILabel alloc]initWithFrame:CGRectMake(100, 5, 150, 33)];
+    label.text =@"Qtde:";
+    
+    UITextField *text = [[UITextField alloc]initWithFrame:CGRectMake(150, 5, 150, 33)];
+    [text.layer setBackgroundColor: [[UIColor whiteColor] CGColor]];
+    [text.layer setBorderColor: [[UIColor grayColor] CGColor]];
+    [text.layer setBorderWidth: 1.0];
+    [text.layer setCornerRadius:8.0f];
+    [text.layer setMasksToBounds:YES];
+    text.contentVerticalAlignment = UIControlContentVerticalAlignmentCenter;
+    text.textAlignment = NSTextAlignmentCenter;
+    text.tag=indexPath.row+1;
+    
+    [celula.contentView addSubview:label];
+    [celula.contentView addSubview:text];
+    
+    return celula;
 }
 
 - (void)didReceiveMemoryWarning
